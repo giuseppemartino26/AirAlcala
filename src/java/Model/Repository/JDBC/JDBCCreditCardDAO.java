6c93fdcf21cd92923874f05c6e393bfd93073b83
pablo <pablo@DESKTOP-QI8QQ82>
2019-01-13 11:01:50 +0100

    Creditcard model

--- src/java/Model/Repository/JDBC/JDBCCreditCardDAO.java
+++ src/java/Model/Repository/JDBC/JDBCCreditCardDAO.java
@@ -7,47 +7,155 @@ package Model.Repository.JDBC;
 
 import Model.CreditCard;
 import Model.Repository.CreditCardDAO;
+import Model.Repository.UserDAO;
+import Model.User;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author fabri
  */
-public class JDBCCreditCardDAO implements CreditCardDAO{
-    
-    private Connection conn;
+public class JDBCCreditCardDAO implements CreditCardDAO {
 
-    public void dbConnect() {
+    private static Connection connObj;
+    private static PreparedStatement stmtObj;
+    private static ResultSet rsObj;
+    private User user;
+    private UserDAO userDAO;
+
+    public Connection dbConnect() {
         try {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
-            conn = DriverManager.getConnection("jdbc:derby://localhost:1527/airAlcala", "root", "root");
+            connObj = DriverManager.getConnection("jdbc:derby://localhost:1527/airAlcala", "root", "root");
             System.out.println("Connected.");
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Not Connected. ");
             System.out.println(e);
         }
+        return connObj;
+    }
+
+    public static void dbDisconnect() {
+        try {
+            rsObj.close();
+            stmtObj.close();
+            connObj.close();
+        } catch (Exception exObj) {
+            exObj.printStackTrace();
+        }
     }
 
     @Override
     public CreditCard find(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        CreditCard cc = null;
+        userDAO = new JDBCUserDAO();
+        String query = "SELECT * FROM creditcard WHERE id = ?";
+        try {
+            connObj = dbConnect();
+            stmtObj = connObj.prepareStatement(query);
+            stmtObj.setInt(1, id);
+            rsObj = stmtObj.executeQuery();
+
+            if (rsObj.next()) {
+                int cc_id = rsObj.getInt("id");
+                int userId = rsObj.getInt("user_id");
+                user = userDAO.find(userId);
+                int number = rsObj.getInt("number");
+                Date expiration = rsObj.getDate("expiration");
+                int cvc = rsObj.getInt("securitycode");
+                cc = new CreditCard(cc_id, user, number, expiration, cvc);
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Not inserted. " + e);
+        }
+        return cc;
     }
 
     @Override
     public boolean insert(CreditCard creditCard) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean inserted = false;
+        int insertedId=0;
+        
+        String query="INSERT INTO creditcard (id, user_id, number, expiration, securitycode)"
+                +"VALUES (?,?,?,?,?)";
+        
+        try {
+            connObj=dbConnect();
+            stmtObj= connObj.prepareStatement(query);
+            stmtObj.setInt(1, creditCard.getId());
+            stmtObj.setInt(2, creditCard.getUser().getId());
+            stmtObj.setInt(3, creditCard.getNumber());
+            stmtObj.setDate(4, (java.sql.Date) creditCard.getExpiration());
+            stmtObj.setInt(5, creditCard.getSecurityCode());
+            
+            insertedId=stmtObj.executeUpdate();
+            
+            dbDisconnect();
+        } catch (SQLException e) {
+            System.out.println("Not inserted. " + e);
+            }
+        if(insertedId>0){
+            inserted=true;
+        }
+        return inserted;
     }
 
     @Override
     public boolean update(CreditCard creditCard) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean updated=false;
+        int updatedId=0;
+        String query = "UPDATE creditcard SET user_id= ?, number= ?, expiration= ?, "
+                +"securitycode= ? WHERE id = ?";
+        
+        try {
+            connObj = dbConnect();
+            stmtObj = connObj.prepareStatement(query);
+            stmtObj.setInt(1, creditCard.getUser().getId());
+            stmtObj.setInt(2, creditCard.getNumber());
+            stmtObj.setDate(3, (java.sql.Date) creditCard.getExpiration());
+            stmtObj.setInt(4, creditCard.getSecurityCode());
+            stmtObj.setInt(5, creditCard.getId());
+            
+            updatedId=stmtObj.executeUpdate();
+            
+            dbDisconnect();
+        } catch (SQLException e) {
+            System.out.println("Not inserted. " + e);
+        }
+        if (updatedId > 0) {
+            updated = true;
+        }
+        return updated;
     }
 
     @Override
     public boolean delete(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean deleted = false;
+        int deletedId = 0;
+        String query = "DELETE FROM creditcard WHERE id=?";
+        try{
+            connObj=dbConnect();
+            stmtObj=connObj.prepareStatement(query);
+            stmtObj.setInt(1, id);
+            
+            deletedId=stmtObj.executeUpdate();
+            
+            dbDisconnect();
+        }catch (SQLException e) {
+            System.out.println("Not inserted. " + e);
+        }
+        if (deletedId > 0) {
+            deleted = true;
+        }
+        return deleted;
     }
-    
+
 }
